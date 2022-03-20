efcab2f03ab04af71f90bc19a44f63b96ec40cee
孟卫平 <mwping1324@163.com>
2022-03-20 16:16:40 +0800

    build: Update dependencies

--- sampleapp/buildSrc/build.gradle.kts
+++ sampleapp/buildSrc/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
 }
 
 dependencies {
-    implementation("org.json:json:20210307")
+    implementation("com.google.firebase:perf-plugin:1.4.1")
+    implementation("com.android.tools.build:gradle:7.1.2")
 }
 repositories {
+    google()
     gradlePluginPortal()
-    jcenter()
     mavenCentral()
 }
